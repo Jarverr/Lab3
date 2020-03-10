02a0e9c82ac9c08b1c95ebfe1761dbe0c84aa04e
Michael <krzyzowskim1998@gmail.com>
2020-03-10 22:05:35 +0100

    first commit

---
+++ Kontekst.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class Kontekst: DbContext //wazne zeby dziedczyło z db context
+    {
+        public DbSet<Student> Students { get; set; } //typ dbset repreznetuje zbior bazodanowy jakas tabela/widok czy coś 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder); 
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"); //pobranie sqlservera zeby moc usesqlserver zrobic i tutaj conneciton string
+        }
+        //protected override void OnModelCreating(ModelBuilder modelBuilder) //fluent(drugi spsob)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder.Entity<Student>() //obiekt pozwlajacy modyfikowac tabele, albo atrybuty albo fluent wzajemnie sie gryza
+        //        .Property(x => x.Imie)
+        //        .HasMaxLength(255)
+        //        .IsRequired()
+        //        .HasColumnName("Name");
+        //    modelBuilder.Entity<Student>()
+        //        .Property(x => x.Nazwisko)
+        //        .HasMaxLength(255);
+        //}
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Lab3
+{
+    public class Student
+    { //opisac baze mozna oprzez adnotacje w klasie required
+        [Key] //jak nazwe to Id to się domysli
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        //[Column("Name")]
+        public string Imie { get; set; }
+        [Required] //to co pod tym ten prop w bazie am byc obowiazkowy 
+        [MaxLength(255)]
+        public string Nazwisko { get; set; }
+    }
+}
